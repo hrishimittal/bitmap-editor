3f643d6cebc026844e45e3e1585a15560e137886
Hrishi Mittal <hrishimittal@gmail.com>
2017-09-15 12:31:31 +0100

    Add method for colouring pixel

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -9,6 +9,11 @@ class BitmapEditor
         m = line[1].to_i
         n = line[2].to_i
         image = create_image(m, n)
+      when 'L'
+        x = line[1].to_i - 1
+        y = line[2].to_i - 1
+        colour = line[3]
+        image = colour_pixel(x, y, colour, image)
       when 'S'
         image = convert_image_matrix_to_string(image)
       else
@@ -20,6 +25,11 @@ class BitmapEditor
 
   def create_image(m, n)
     Array.new(n){Array.new(m,'O')}
+  end
+
+  def colour_pixel(x, y, colour, image)
+    image[y][x] = colour
+    image
   end
 
   def convert_image_matrix_to_string(image)
