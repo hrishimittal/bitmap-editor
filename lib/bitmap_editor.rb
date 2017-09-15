673ea2f9d4fc562bace95178b7ed952cbfecc003
Hrishi Mittal <hrishimittal@gmail.com>
2017-09-15 11:39:57 +0100

    Rename output and refactor create image method

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,26 +1,24 @@
 class BitmapEditor
   def run(file)
     return puts 'please provide correct file' if file.nil? || !File.exists?(file)
-    output = ''
+    image = ''
     File.open(file).each do |line|
       line = line.chomp.split(" ")
       case line[0]
       when 'I'
         m = line[1].to_i
         n = line[2].to_i
-        output = create_image(m, n, output)
+        image = create_image(m, n)
       when 'S'
-        output = 'There is no image' if output.empty?
+        image = image.empty? ? 'There is no image' : image.map{|r| r.join }.join("\n")
       else
-        output = 'unrecognised command :('
+        image = 'unrecognised command :('
       end
     end
-    puts output
+    puts image
   end
 
-  def create_image(m, n, output)
-    b = Array.new(n){Array.new(m,'O')}
-    output += b.map{|r| r.join }.join("\n")
-    output
+  def create_image(m, n)
+    Array.new(n){Array.new(m,'O')}
   end
 end
