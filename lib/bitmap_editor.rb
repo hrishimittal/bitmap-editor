d3f25105a77893b6239746c99d17bb0dce017268
Hrishi Mittal <hrishimittal@gmail.com>
2017-09-15 12:23:33 +0100

    refactor S command

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,7 +1,7 @@
 class BitmapEditor
   def run(file)
     return puts 'please provide correct file' if file.nil? || !File.exists?(file)
-    image = ''
+    image = 'There is no image'
     File.open(file).each do |line|
       line = line.chomp.split(" ")
       case line[0]
@@ -10,7 +10,7 @@ class BitmapEditor
         n = line[2].to_i
         image = create_image(m, n)
       when 'S'
-        image = image.empty? ? 'There is no image' : image.map{|r| r.join }.join("\n")
+        image = convert_image_matrix_to_string(image)
       else
         image = 'unrecognised command :('
       end
@@ -20,5 +20,10 @@ class BitmapEditor
 
   def create_image(m, n)
     Array.new(n){Array.new(m,'O')}
+  end
+
+  def convert_image_matrix_to_string(image)
+    return unless image.is_a? Array
+    image.map{|r| r.join }.join("\n")
   end
 end
