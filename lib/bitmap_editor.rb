5d06cd100930cbf7c3095a54d881e88d5f7f83ba
Hrishi Mittal <hrishimittal@gmail.com>
2017-09-15 10:22:36 +0100

    fix quotes and formatting

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,13 +1,12 @@
 class BitmapEditor
-
   def run(file)
-    return puts "please provide correct file" if file.nil? || !File.exists?(file)
+    return puts 'please provide correct file' if file.nil? || !File.exists?(file)
 
     File.open(file).each do |line|
       line = line.chomp
       case line
       when 'S'
-          puts "There is no image"
+          puts 'There is no image'
       else
           puts 'unrecognised command :('
       end
