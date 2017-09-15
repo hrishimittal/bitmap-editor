0e258faec70835515bd8260eaa89f2f17b8c1b25
Hrishi Mittal <hrishimittal@gmail.com>
2017-09-15 11:25:13 +0100

    Add spec for command L for colouring a pixel

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -3,11 +3,30 @@ require './lib/bitmap_editor.rb'
 describe 'I' do
   context 'when given two integers as arguments' do
     let(:output) { `bin/bitmap_editor ./examples/create_image.txt` }
+
     it 'creates an M x N image with colour white (O)' do
       expect(output).to eq(<<~EOS
                               OOOOO
                               OOOOO
                               OOOOO
+                              OOOOO
+                              OOOOO
+                              OOOOO
+                            EOS
+                            )
+    end
+  end
+end
+
+describe 'L' do
+  context 'when given 3 arguments X, Y and C' do
+    let(:output) { `bin/bitmap_editor ./examples/colour_pixel.txt` }
+
+    it 'colours the pixel (X,Y) with colour C' do
+      expect(output).to eq(<<~EOS
+                              OOOOO
+                              OOOOO
+                              AOOOO
                               OOOOO
                               OOOOO
                               OOOOO
