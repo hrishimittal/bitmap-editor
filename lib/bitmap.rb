d46278946e34f46d85a46b34658982c3714d2882
Hrishi Mittal <hrishimittal@gmail.com>
2017-09-18 14:40:50 +0100

    Fix Rubocop offenses in Bitmap

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -6,30 +6,33 @@ class Bitmap
   end
 
   def create_pixels(width, height)
-    fail StandardError, "Out of bounds" if (out_of_bounds?(width) || out_of_bounds?(height))
-    Array.new(height){Array.new(width,'O')}
+    fail StandardError, 'Out of bounds' if out_of_bounds?(width) ||
+                                           out_of_bounds?(height)
+    Array.new(height) { Array.new(width, 'O') }
   end
 
   def format_pixels
-    @pixels.map{|row| row.join }.join("\n")
+    @pixels.map(&:join).join("\n")
   end
 
   def clear
-    @pixels.map{ |row| row.fill('O') }
+    @pixels.map { |row| row.fill('O') }
   end
 
   def set_horizontal_segment_colour(x1, x2, y, colour)
-    if out_of_bounds?(x1, @width) || out_of_bounds?(x2, @width) || out_of_bounds?(y, @height)
-      fail StandardError, "Segment out of bounds"
+    if out_of_bounds?(x1, @width) || out_of_bounds?(x2, @width) ||
+       out_of_bounds?(y, @height)
+      fail StandardError, 'Segment out of bounds'
     end
     @pixels[y - 1].fill(colour, (x1 - 1)..(x2 - 1))
   end
 
   def set_vertical_segment_colour(x, y1, y2, colour)
-    if out_of_bounds?(x, @width) || out_of_bounds?(y2, @height) || out_of_bounds?(y2, @height)
-      fail StandardError, "Segment out of bounds"
+    if out_of_bounds?(x, @width) || out_of_bounds?(y2, @height) ||
+       out_of_bounds?(y2, @height)
+      fail StandardError, 'Segment out of bounds'
     end
-    @pixels[(y1 - 1)..(y2 - 1)].map{ |row| row[x - 1] = colour }
+    @pixels[(y1 - 1)..(y2 - 1)].map { |row| row[x - 1] = colour }
   end
 
   def set_pixel_colour(x, y, colour)
@@ -40,7 +43,8 @@ class Bitmap
   end
 
   private
-    def out_of_bounds?(dimension, max = 250)
-      dimension < 1 || dimension > max
-    end
+
+  def out_of_bounds?(dimension, max = 250)
+    dimension < 1 || dimension > max
+  end
 end
