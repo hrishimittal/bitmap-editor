500ec309c2f4d21e97e7bf99c870eda064493853
Hrishi Mittal <hrishimittal@gmail.com>
2017-09-18 15:27:41 +0100

    Replace double quotes with single quotes

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -7,29 +7,29 @@ describe BitmapEditor do
 
   context 'when no bitmap has been created' do
     it 'raises error when running the L command' do
-      expect { @bitmap_editor.execute(['L', 1, 2, 'O']) }.to raise_error(StandardError, "No bitmap found")
+      expect { @bitmap_editor.execute(['L', 1, 2, 'O']) }.to raise_error(StandardError, 'No bitmap found')
     end
 
     it 'raises error when running the S command' do
-      expect { @bitmap_editor.execute(['S']) }.to raise_error(StandardError, "No bitmap found")
+      expect { @bitmap_editor.execute(['S']) }.to raise_error(StandardError, 'No bitmap found')
     end
 
     it 'raises error when running the C command' do
-      expect { @bitmap_editor.execute(['C']) }.to raise_error(StandardError, "No bitmap found")
+      expect { @bitmap_editor.execute(['C']) }.to raise_error(StandardError, 'No bitmap found')
     end
 
     it 'raises error when running the V command' do
-      expect { @bitmap_editor.execute(['V', 1, 2, 3, 'O']) }.to raise_error(StandardError, "No bitmap found")
+      expect { @bitmap_editor.execute(['V', 1, 2, 3, 'O']) }.to raise_error(StandardError, 'No bitmap found')
     end
 
     it 'raises error when running the H command' do
-      expect { @bitmap_editor.execute(['H', 1, 2, 3, 'O']) }.to raise_error(StandardError, "No bitmap found")
+      expect { @bitmap_editor.execute(['H', 1, 2, 3, 'O']) }.to raise_error(StandardError, 'No bitmap found')
     end
   end
 
   describe 'Unrecognised command' do
     it 'raises Unrecognised command error' do
-      expect { @bitmap_editor.execute(['X'])  }.to raise_error(StandardError, "Unrecognised command: X")
+      expect { @bitmap_editor.execute(['X'])  }.to raise_error(StandardError, 'Unrecognised command: X')
     end
   end
 
